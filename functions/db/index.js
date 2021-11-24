471770859952550327c16d900b026fadf9947d66
nahee-park <jessica396@ewhain.net>
2021-11-25 04:43:51 +0900

    dev: db connect

---
+++ functions/db/index.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//     carDB: require('./car'),
+//     carLocationDB: require('./carLocation'),
+//     carPriceDB: require('./carPrice'),
+//     likeDB: require('./like'),
+//     reserveDB: require('./reserve'),
+//     userDB: require('./user'),
+//   };
